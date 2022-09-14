f97f89bcdee2244ffc5bb0670b6200ebc6b9697f
tu204f <david_yusupov@mail.ru>
2022-09-14 17:54:32 +0300

    тестовое задание Калькулятор_Java /Оставляем/

--- sample/MyClass/src/MyClass.java
+++
@@ -1,9 +0,0 @@
-public class MyClass {
-    public static void main(String[] args){
-        String s1 = new String("123");
-        String s2 = new String("123");
-        System.out.println(s1==s2);
-
-
-    }
-}
--- sample/gui/src/From.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.*;
-
-public class From {
-    private JPanel rootPanel;
-    private JButton button1;
-}
