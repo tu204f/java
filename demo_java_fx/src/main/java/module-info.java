4f738049428e0c39b8544d24cba9fd23a296b07a
tu204f <david_yusupov@mail.ru>
2022-09-14 17:52:47 +0300

    no message

--- demo_java_fx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demo_java_fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.demo_java_fx to javafx.fxml;
-    exports com.example.demo_java_fx;
-}
